37423a3eaafeff5c25caddb340f8df2e40b6080c
xyzHooDlEzyx <93081557+xyzHooDlEzyx@users.noreply.github.com>
2024-02-27 16:41:26 +0200

    Lab№1 (#1)
    * додав код, що
    
    * написав код для лабораторної
    
    код виконує поставлену умову, є перевірка даного коду через юніттест
    
    * Правки коду, зміна алгоритму сортування
    
    Алгоритм сортування було змінено з Selection sort на Quick sort, unittest-и були перероблені через зміну алгоритму, виправлення коду згідно pep8

---
+++ Lab№1/lab1_var3.py
@@ -0,0 +1,36 @@
+class KIsGreaterThenSizeOfArr (Exception):
+    def __init__(self, message='K is greater then size of input array'):
+        self.message = message
+        super().__init__(self.message)
+
+
+def set_pivot(elem, left, right):
+    pivot = elem[right]
+    i = left - 1
+    for j in range(left, right):
+        if elem[j] >= pivot:
+            i += 1
+            elem[i], elem[j] = elem[j], elem[i]
+    elem[i+1], elem[right] = elem[right], elem[i+1]
+    return i + 1
+
+
+def quick_select(elem, left, right, k):
+    if left <= right:
+        pivot_idx = set_pivot(elem, left, right)
+        if pivot_idx == k:
+            return elem[pivot_idx]
+        elif pivot_idx < k:
+            return quick_select(elem, pivot_idx + 1, right, k)
+        else:
+            return quick_select(elem, left, pivot_idx - 1, k)
+
+
+def find_k_or_error(elem, k):
+    if k < 1 or k > len(elem):
+        raise KIsGreaterThenSizeOfArr()
+    return quick_select(elem, 0, len(elem)-1, k-1)
+
+
+if __name__ == '__main__':
+    print(find_k_or_error([3, 2, 1, 5, 6, 4], 3))
---
+++ Lab№1/lab1_var3_test.py
@@ -0,0 +1,19 @@
+import unittest
+from lab1_var3 import KIsGreaterThenSizeOfArr, find_k_or_error
+
+
+class TestKthLargestElement(unittest.TestCase):
+    def test_find_kth_largest(self):
+        self.assertEqual(find_k_or_error([3, 2, 1, 5, 6, 4], 1), 6)
+        self.assertEqual(find_k_or_error([3, 2, 1, 5, 6, 4], 2), 5)
+        self.assertEqual(find_k_or_error([3, 2, 1, 5, 6, 4], 3), 4)
+
+    def test_k_out_of_bounds(self):
+        with self.assertRaises(KIsGreaterThenSizeOfArr):
+            find_k_or_error([3, 2, 1, 5, 6, 4], 0)
+        with self.assertRaises(KIsGreaterThenSizeOfArr):
+            find_k_or_error([3, 2, 1, 5, 6, 4], 7)
+
+
+if __name__ == '__main__':
+    unittest.main()
