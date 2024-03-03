24f7091674c4c590a3ab6709e426e9d4f5fbe7d5
xyzHooDlEzyx <93081557+xyzHooDlEzyx@users.noreply.github.com>
2024-03-03 12:48:51 +0200

    Код до 2 лаби та тест до неї
    Код що виконує умови 2-ої лабораторної роботи та юніттест до неї

---
+++ Lab№2/lab2_var3_tests.py
@@ -0,0 +1,20 @@
+import unittest
+from lab2_var3 import min_eating_speed, JackieWillBeCaught
+
+
+class TestMinEatingSpeed(unittest.TestCase):
+
+    def test_examples(self):
+        self.assertEqual(min_eating_speed([3, 6, 7, 11], 8), 4)
+
+        self.assertEqual(min_eating_speed([30, 11, 23, 4, 20], 5), 30)
+
+        self.assertEqual(min_eating_speed([30, 11, 23, 4, 20], 6), 23)
+
+    def test_error_raise(self):
+        with self.assertRaises(JackieWillBeCaught):
+            min_eating_speed([3, 2, 1, 5, 6, 4], 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
