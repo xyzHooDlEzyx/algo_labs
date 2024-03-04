05bb9eb4dbe55e6c5c7e7d642247de28eef44972
xyzHooDlEzyx <93081557+xyzHooDlEzyx@users.noreply.github.com>
2024-03-04 23:40:41 +0200

    код до 2 лаби та тест
    написав код до 2 лабораторної, що виконує її умови, та юніттест до неї

---
+++ Lab№2/lab2.py
@@ -0,0 +1,27 @@
+class JackieWillBeCaught(Exception):
+    def __init__(self, message='Jackie does not have enough time to eat bananas'):
+        self.message = message
+        super().__init__(self.message)
+
+
+def min_eating_speed(piles, h):
+    if h < len(piles):
+        raise JackieWillBeCaught
+
+    def possible(k):
+        return sum((pile - 1) // k + 1 for pile in piles) <= h
+
+    minimum, maximum = 1, max(piles)
+
+    while maximum > minimum:
+        middle = (minimum + maximum) // 2
+        if possible(middle):
+            maximum = middle
+        else:
+            minimum = middle + 1
+    return minimum
+
+
+if __name__ == "__main__":
+    arr = [1, 12, 4, 6, 8]
+    print(min_eating_speed(arr, 5))
