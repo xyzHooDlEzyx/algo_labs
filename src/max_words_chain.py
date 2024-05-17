ec18cfdbe58e22ea99ac2aa0503814a1ed292a91
xyzHooDlEzyx <93081557+xyzHooDlEzyx@users.noreply.github.com>
2024-05-17 13:54:11 +0300

    код до 9 лаби та тест
    додав код до  9 лабораторної, тест до неї обновив readme.md

---
+++ src/max_words_chain.py
@@ -0,0 +1,28 @@
+def max_chain_length(in_list):
+    in_list.sort(key=len)
+    dp = {}
+
+    max_length = 1
+
+    for word in in_list:
+        dp[word] = 1
+        for i in range(len(word)):
+            prev = word[:i] + word[i + 1:]
+            print(f"word: {word}, prev: {prev}")
+            if prev in in_list:
+                dp[word] = max(dp[word], dp[prev] + 1)
+        max_length = max(max_length, dp[word])
+
+    return max_length
+
+
+def read_input(path_to_infile):
+    with open(path_to_infile, "r") as infile:
+        n = int(infile.readline().strip())
+        list_of_words = [infile.readline().strip() for _ in range(n)]
+    return list_of_words
+
+
+def write_output(result, path_to_outfile):
+    with open(path_to_outfile, "w") as outfile:
+        outfile.write(str(result) + "\n")
---
+++ test/max_words_chain_test.py
@@ -0,0 +1,51 @@
+import unittest
+
+from src.max_words_chain import read_input, write_output, max_chain_length
+
+
+class MaxWordChainTest(unittest.TestCase):
+    def setUp(self):
+        self.input_file = "../test/source/wchain.in"
+        self.output_file = "../test/source/wchain.out"
+
+    def test_max_chain_length(self):
+        words = read_input(self.input_file)
+
+        expected_result = 6
+
+        result = max_chain_length(words)
+
+        self.assertEqual(result, expected_result)
+
+    def test_read_input(self):
+        expected_words = [
+            "crates",
+            "car",
+            "cat",
+            "cats",
+            "crate",
+            "rate",
+            "at",
+            "ate",
+            "tea",
+            "rat",
+            "a",
+        ]
+
+        words = read_input(self.input_file)
+
+        self.assertEqual(words, expected_words)
+
+    def test_write_output(self):
+        result = 6
+
+        write_output(result, self.output_file)
+
+        with open(self.output_file, "r") as file:
+            output = int(file.read().strip())
+
+        self.assertEqual(output, result)
+
+
+if __name__ == "__main__":
+    unittest.main()
